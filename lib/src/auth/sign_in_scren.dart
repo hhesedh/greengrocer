f54d3827f32c7821b3af4b1451259ccd23367c3f
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-09-04 20:32:50 -0300

    adição de input de texto na tela de login

--- lib/src/auth/sign_in_scren.dart
+++ lib/src/auth/sign_in_scren.dart
@@ -16,11 +16,30 @@ class SignInScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
+              ),
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(
                   top: Radius.circular(45),
                 ),
+              ),
+              child: Column(
+                children: [
+                  // Email
+                  TextFormField(
+                    decoration: InputDecoration(
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                  ),
+                  // Senha
+                  TextFormField(),
+                ],
               ),
             ),
           ),
