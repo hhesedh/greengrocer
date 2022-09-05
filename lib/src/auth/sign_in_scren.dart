bafb4eddadde35b88cb4964da1d6f0b1ba30f3ae
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-09-04 21:22:00 -0300

    criação de visibilidade para senhas

--- lib/src/auth/sign_in_scren.dart
+++ lib/src/auth/sign_in_scren.dart
@@ -39,7 +39,7 @@ class SignInScreen extends StatelessWidget {
                   CustomTextField(
                     icon: Icons.lock,
                     label: 'Senha',
-                    isObscure: true,
+                    isSecret: true,
                   ),
                 ],
               ),
